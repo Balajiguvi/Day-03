2141a9c44adc25d128fa66af396be60ca28d3257
Balajiguvi <107595021+Balajiguvi@users.noreply.github.com>
2022-09-16 19:33:17 +0530

    day-03 task resume

--- json resume.js
+++ json resume.js
@@ -1,35 +1,35 @@
-var Resume={
-    Basicinformation:{
-        Name:'Balaji.G',
-        Age:24,
-        email:'balaviji@gmail.com',
-        phonenumber:7538880784
+var resume={
+    "Basicinformation":{
+        "Name":"Balaji.G",
+        "Age":"24",
+        "email":"balaviji@gmail.com",
+        "phonenumber":"7538880784"
     },
-    WorkAndExperience:'I am a fresher,willing to work in the role of full stack developer',
-    Education:[
+    "WorkAndExperience":"I am a fresher,willing to work in the role of full stack developer",
+    "Education":[
         {
-            School:'government heigher secondary school,bhuvanagiri,608601',
-            'year of 10th pass':2014,
-            'year of 12th pass':2016,
+            "School":"government heigher secondary school,bhuvanagiri,608601",
+            "year of 10th pass":"2014",
+            "year of 12th pass":"2016"
         },
         {
-            college:'university college of engineering,pattukottai',
-            degree:'bachelor of engineering',
-            department:'EEE',
-            'year of passed out':2020
+            "college":"university college of engineering,pattukottai",
+            "degree":"bachelor of engineering",
+            "department":"EEE",
+            "year of passed out":"2020"
         }
     ],
-    Skills:[
-        '1.good knowledge in html,css javascript',
-        '2.quick leaner',
-        '3.good knowledge in frontend language'
+    "Skills":[
+        "1.good knowledge in html,css javascript",
+        "2.quick leaner",
+        "3.good knowledge in frontend language"
     ],
-    Languagesknown:['Tamil,english'],
-    Intrest:[
-        '1.learn coding',
-        '2.create browser',
-        '3.hearing songs'
-    ],
+    "Languagesknown":["Tamil,english"],
+    "Intrest":[
+        "1.learn coding",
+        "2.create browser",
+        "3.hearing songs"
+    ]
 }
 
 let Resumejson=JSON.stringify(Resume);
